20d3b121b98baf169e8fe86e6e9c5a3ce7a51961
Leena-elsaadan <s-leena.elsaadani@zewailcity.edu.eg>
2026-03-07 17:36:00 +0200

    Add api.js: axios instance with JWT interceptor

---
+++ src/api/api.js
@@ -0,0 +1,34 @@
+// src/api/api.js
+
+import axios from "axios";
+
+// Axios instance
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+});
+
+// Request interceptor: attach JWT token if it exists
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Response interceptor: handle 401 Unauthorized
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
